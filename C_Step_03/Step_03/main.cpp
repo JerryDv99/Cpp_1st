210fb2c760dc5de5edb35c961fb7cdbdfe961ba4
Jerry <devmanjerry@gmail.com>
2022-04-01 20:46:58 +0900

    Step_03

---
+++ C_Step_03/Step_03/main.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main(void)
+{
+	////	변수 선언
+	//int a;
+	//int b;					
+
+	////	선언과 동시에 초기화
+	//int c = 30, d = 40;		
+
+	////	초기화
+	//a = 10;
+	//b = 20;					
+
+	//printf_s("%d %d\n", a, b);
+	//printf_s("%d %d\n", c, d);
+
+		
+	/*
+	//	 변수 선언
+	float fNumber;
+	//	변수 초기화
+	fNumber = 87.654321;
+	//	출력
+	printf_s("%f\n", fNumber);
+	//	실수형 타입 변수를 정수형 타입 변수에 대입하면 소수점 데이터는 소실
+	printf_s("%d\n", (int)fNumber); 
+	printf_s("%c\n", (char)fNumber);
+	printf_s("%d\n", fNumber);*/
+
+	//	float 타입 변수 초기화 시 f를 붙여주지 않으면 double이 됨
+	/*float f = 1.12345f;
+
+	printf_s("%.2f\n", f);*/
+
+	//printf_s("%d\n", 3.14);			  // 1374389535
+	//printf_s("%d\n", 314);			  // 314
+	//printf_s("%d\n", 0x13a);		  // 314
+	//								  
+	//printf_s("%x\n", 0x13a);		  // 13a
+	//printf_s("%X\n", 0X13A);		  // 13A
+
+	//printf_s("%d\n", 'A');		// 65
+	//printf_s("%c\n", 65);			// A
+	//printf_s("%x\n", 'A');		// 41
+	//printf_s("%c\n", 'A');		// A
+
+	/*
+	int iNum;		  // 값을 저장 받을 int형
+	float fNum;		  // 값을 저장 받을 float형
+
+	printf_s("iNum 값 입력 : ");
+	scanf_s("%d", &iNum);	// scanf_s(); 에 값을 입력하고 **'&' 로 저장 될 위치를 정해준다
+
+	printf_s("fNum 값 입력 : ");
+	scanf_s("%f", &fNum);
+
+	printf_s("iNum : %d\n", iNum);
+	printf_s("fNum : %f\n", fNum);
+	*/
+
+	return 0;
+}
