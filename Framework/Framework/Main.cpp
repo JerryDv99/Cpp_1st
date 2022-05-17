4acc3fc37ec3348cf2c51048f95249e7e332941b
Jerry <devmanjerry@gmail.com>
2022-05-17 20:47:12 +0900

    Framework 0.2

--- Framework/Framework/Main.cpp
+++ Framework/Framework/Main.cpp
@@ -1,5 +1,8 @@
+// Framework v 0.2
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>  // 입출력 헤더
+#include <string>
 
 using namespace std;	// iostream과 세트
 
@@ -21,6 +24,50 @@ using namespace std;	// iostream과 세트
 //
 //
 //}INFO;
+
+
+typedef struct Vector3
+{
+	int x = 0, y = 0, z = 0;
+
+	Vector3() {};	//	기본 생성자 없이는
+
+	Vector3(int _x, int _y)	// 복사 생성자를 만들 수 없다
+		: x(_x), y(_y), z(0) { };
+
+	Vector3(int _x, int _y, int _z)	
+		: x(_x), y(_y), z(_z) { };
+
+
+}Vector3;
+
+
+struct Transform
+{
+	// 오브젝트가 존재하기 위해 필요한 최소한의 요소들
+	Vector3 Position;
+	Vector3 Rotation;
+	Vector3 Scale;
+};
+
+struct Object
+{
+	char* Name;
+	int Speed;
+	Transform TramsInfo;
+
+};
+
+// 초기화 함수
+void Initialize(Object* _Object, char* _Name, int _PosX = 0, int _PosY = 0, int _PosZ = 0);
+
+// 이름 세팅 함수
+char* SetName();
+
+// 출력 함수
+void Output(Object* _Object);
+
+
 
 int main(void)
 {
@@ -73,5 +120,71 @@ int main(void)
 	//for (int i = 0; i < 10; ++i)
 		//cout << pInfo[i]->iNumber << endl;
 
+	//Vector3 vPosition = Vector3(10, 20, 30);	// 매개변수의 개수나 형태에 따라 호출
+
+	//cout << vPosition.x << ", " << vPosition.y << endl;
+
+	// ** 플레이어 선언 및 동적할당
+	Object* Player = new Object;
+
+	// ** 플레이어 초기화
+	Initialize(Player, nullptr, 10, 20, 30);
+
+	Object* Enemy = new Object;
+	Initialize(Enemy, (char*)"Enemy", 100, 200, 300);
+
+	Output(Player);
+	Output(Enemy);
+
 	return 0;
+}
+
+char* SetName()
+{
+	// 포인터 변수에는 바로 입력받을 수 없기 때문에 문자열을 입력 받을 임시 변수를 배열로 생성 
+	char Buffer[128] = "";
+	
+	cout << "입력 : "; cin >> Buffer;
+
+	// 포인터 변수를 선언하여 입력받은 문자열의 길이 + null 만큼의 크기를 할당
+	//char* pName = (char*)malloc(strlen(Buffer) + 1);
+	char* pName = new char[strlen(Buffer) + 1];
+
+	// 입력받은 문자열의 내용 복사
+	strcpy(pName, Buffer);
+
+	// char* 반환
+	return pName;
+}
+
+void Initialize(Object* _Object, char* _Name, int _PosX, int _PosY, int _PosZ)
+{
+	// ** 3항 연산자
+	// _Name의 값이 nullptr이면 SetName()함수 실행
+	// 아니라면 _Name 값 대입
+	_Object->Name = (_Name == nullptr) ? SetName() : _Name;
+
+	_Object->Speed = 0;
+
+	// ** 좌표값
+	_Object->TramsInfo.Position = Vector3(_PosX, _PosY, _PosZ);
+
+	// ** 회전값 (현재 사용 안함)
+	_Object->TramsInfo.Rotation = Vector3(0, 0, 0);
+
+	// ** 크기값
+	_Object->TramsInfo.Scale = Vector3(0, 0, 0);
+
+}
+void Output(Object* _Object)
+{
+	if (_Object->Name != nullptr)
+		cout << "Name : " << _Object->Name << endl;
+
+	cout << "Speed : " << _Object->Speed << endl;
+
+	cout << "X : " << _Object->TramsInfo.Position.x <<
+		", Y : " << _Object->TramsInfo.Position.y <<
+		", Z : " << _Object->TramsInfo.Position.z << endl;
+
 }
